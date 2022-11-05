457544e13f4cd1c77e081d55b4e0d5c6cb86ef0a
Jan Bolting <jan.bolting@gmail.com>
2022-11-05 12:11:11 +0100

    (#4) Adds first unit tests and JSON example file generator

--- minimumLovablePrototype/csv_example.py
+++ minimumLovablePrototype/csv_example.py
@@ -1,10 +1,26 @@
 import csv
+import os
+from pathlib import Path
 
 import example_observations
 
-single_observation_dict = example_observations.generate()
-# write single epoch to csv file
-with open('example.csv', 'w',newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=list(single_observation_dict))
-    writer.writeheader()
-    writer.writerow(single_observation_dict)
+
+def generate_example_file(
+        file_path=Path('__file__').resolve().parent.joinpath(Path("example.csv"))
+) -> Path:
+    header, observations = example_observations.generate()
+    # write observations to csv file
+    if file_path.exists():
+        print(f"Removed existing CSV example file: {file_path}")
+        os.remove(file_path)
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=list(observations[0]))
+        writer.writeheader()
+        for observation in observations:
+            writer.writerow(observation)
+    print(f"Generated CSV example file: {file_path}")
+    return file_path
+
+
+if __name__ == "__main__":
+    generate_example_file()
--- minimumLovablePrototype/example_observations.py
+++ minimumLovablePrototype/example_observations.py
@@ -3,29 +3,46 @@ import csv
 
 def generate():
     # create single log as dictionary
-    return { \
-        'tai_s': 1662855733.123456789, \
-        'constellation': 'G', \
-        'prn': 56, \
-        'observation_code': '1C', \
-        'code_observation_m': 20685773.805123, \
-        'doppler_observation_hz': -1214.816, \
-        'carrier_observation_m': 20685780.9518393, \
-        'cn0_dbhz': 48.9, \
-        'satellite_position_x_m': 13788686.065826, \
-        'satellite_position_y_m': 6986730.9310050, \
-        'satellite_position_z_m': 21551023.534684, \
-        'satellite_velocity_x_mps': -601.654533296824, \
-        'satellite_velocity_y_mps': 2681.16549309343, \
-        'satellite_velocity_z_mps': -473.157677799463, \
-        'satellite_clock_bias_m': -42073.746123, \
-        'satellite_clock_bias_drift_mps': -0.003238, \
-        'group_delay_s': 0.558407, \
-        'sagnac_effect_m': -7.463553, \
-        'tropo_delay_m': 2.612978, \
-        'iono_delay_m': 1.64424645061389, \
-        'constellation_time_minus_tai_s': 19, \
-        'approximate_antenna_position_x_m': 4627869.62515177, \
-        'approximate_antenna_position_y_m': 119635.618137254, \
-        'approximate_antenna_position_z_m': 4373009.98713997, \
+    header = {
+        "speed_of_light_mps": 299792458.123456,
+        "input_files": [
+            {"name": "file1.obs", "md5": "9e107d9d372bb6826bd81d3542a419d6"},
+            {"name": "file2.nav", "md5": "zjutrd9d372bb6826bd81d3542a47j43"}
+        ],
+        "reference_frame": "ITRF2008@2005.0",
+        "carrier_frequencies": [
+            {"constellation": "G", "signal": "L1C",
+             "frequency": "1575420000.000000"}
+        ],
+        "prx_git_commit_id": "663524c95079f0bf3a91ef0d7efd7abe97f82a76"
     }
+    observations = []
+    observations.append({
+        'tai_s': 1662855733.123456789,
+        'constellation': 'G',
+        'prn': 56,
+        'observation_code': '1C',
+        'code_observation_m': 20685773.805123,
+        'doppler_observation_hz': -1214.816,
+        'carrier_observation_m': 20685780.9518393,
+        'cn0_dbhz': 48.9,
+        'satellite_position_x_m': 13788686.065826,
+        'satellite_position_y_m': 6986730.9310050,
+        'satellite_position_z_m': 21551023.534684,
+        'satellite_velocity_x_mps': -601.654533296824,
+        'satellite_velocity_y_mps': 2681.16549309343,
+        'satellite_velocity_z_mps': -473.157677799463,
+        'satellite_clock_bias_m': -42073.746123,
+        'satellite_clock_bias_drift_mps': -0.003238,
+        'group_delay_s': 0.558407,
+        'sagnac_effect_m': -7.463553,
+        'tropo_delay_m': 2.612978,
+        'iono_delay_m': 1.64424645061389,
+        'constellation_time_minus_tai_s': 19,
+        'approximate_antenna_position_x_m': 4627869.62515177,
+        'approximate_antenna_position_y_m': 119635.618137254,
+        'approximate_antenna_position_z_m': 4373009.98713997,
+    })
+    observations.append(observations[0].copy())
+    observations[1]["observation_code"] = "2C"
+    return header, observations
