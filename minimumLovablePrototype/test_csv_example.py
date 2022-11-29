fc3a8568d5b0327bb60516ed696430491b8b9b87
Jan Bolting <jan.bolting@swift-nav.com>
2022-11-29 08:43:33 +0100

    fixes warnings (calling randint with non-integer arguments)

--- minimumLovablePrototype/test_csv_example.py
+++ minimumLovablePrototype/test_csv_example.py
@@ -9,7 +9,7 @@ import csv_example
 def test_csv_file_generated():
     random.seed(time.time())
     # Leverage the file name argument to generate a file with a unique name and delete it right afterwards:
-    test_file_path = Path('__file__').resolve().parent.joinpath(Path(f"tmp_test_file_{random.randint(0, 1e12)}.csv"))
+    test_file_path = Path('__file__').resolve().parent.joinpath(Path(f"tmp_test_file_{random.randint(int(0), int(1e12))}.csv"))
     # Be extra careful and check that the file does not already exist before we try to generate it:
     assert not test_file_path.exists()
     csv_example.generate_example_file(test_file_path)
--- minimumLovablePrototype/test_json_example.py
+++ minimumLovablePrototype/test_json_example.py
@@ -9,7 +9,7 @@ import json_example
 def test_json_file_generated():
     random.seed(time.time())
     # Leverage the file name argument to generate a file with a unique name and delete it right afterwards:
-    test_file_path = Path('__file__').resolve().parent.joinpath(Path(f"tmp_test_file_{random.randint(0, 1e12)}.jsonl"))
+    test_file_path = Path('__file__').resolve().parent.joinpath(Path(f"tmp_test_file_{random.randint(int(0), int(1e12))}.jsonl"))
     # Be extra careful and check that the file does not already exist before we try to generate it:
     assert not test_file_path.exists()
     json_example.generate_example_file(test_file_path)
