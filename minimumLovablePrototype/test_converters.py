23e1df5584f88b1d69162215ebb4a3457797b5b4
Jan Bolting <jan.bolting@swift-nav.com>
2023-01-27 08:37:30 +0100

    Has unit test actually check whether expected files are generated.

--- minimumLovablePrototype/test_converters.py
+++ minimumLovablePrototype/test_converters.py
@@ -10,9 +10,17 @@ import prx
 
 def test_compressed_crx_to_rnx():
     test_directory = Path(f"./tmp_test_directory_{__name__}").resolve()
-    os.makedirs(test_directory, exist_ok=True)
+    if test_directory.exists():
+        # Make sure the expected files have not been generated before and are still on disk due to e.g. a previous
+        # test run having crashed:
+        shutil.rmtree(test_directory)
+    os.makedirs(test_directory)
     compressed_compact_rinex_file = "TLSE00FRA_R_20230010000_10S_01S_MO.crx.gz"
     shutil.copy(prx.prx_root().joinpath(f"datasets/{compressed_compact_rinex_file}"),
                 test_directory.joinpath(compressed_compact_rinex_file))
     converters.anything_to_rinex_3(test_directory.joinpath(compressed_compact_rinex_file))
+    expected_uncompressed_file = test_directory.joinpath(compressed_compact_rinex_file.replace('.gz', ''))
+    expected_uncompacted_file = test_directory.joinpath(compressed_compact_rinex_file.replace('crx.gz', 'rnx'))
+    assert expected_uncompressed_file.exists()
+    assert expected_uncompacted_file.exists()
     shutil.rmtree(test_directory)
