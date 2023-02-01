8992c841f6f56770bf3f7732892cd645d2dffa9f
Jan Bolting <jan.bolting@swift-nav.com>
2023-02-01 09:51:41 +0100

    Fixes bug and adds new constants

--- minimumLovablePrototype/constants.py
+++ minimumLovablePrototype/constants.py
@@ -2,5 +2,8 @@ import datetime
 
 cGpstEpoch = datetime.datetime(1980, 1, 6, 0, 0, 0)
 cNanoSecondsPerSecond = 1e-9
-cGpsIcdSpeedOfLight_mps = 2.99792458 * 10e8
+cHzPerMhz = 1e6
+cGpsIcdSpeedOfLight_mps = 2.99792458 * 1e8
 cPrxReferenceFrame = {"name": "IGS14", "epoch": "2005.001"}
+
+cPrxJsonTextSequenceFileExtension = "jsonseq"
