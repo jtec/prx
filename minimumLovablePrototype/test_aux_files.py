e31bc60f6f2775581773e951d79366d231aaff3d
Jan Bolting <jan.bolting@swift-nav.com>
2023-02-09 20:32:10 +0100

    Use pytest fixture to clean up after test.

--- minimumLovablePrototype/test_aux_files.py
+++ minimumLovablePrototype/test_aux_files.py
@@ -1,35 +1,25 @@
 import os
 from pathlib import Path
 import shutil
-import subprocess
+import pytest
 import prx
-
-import constants
 import aux_files as aux
 
 
-def test_directory():
-    return Path(f"./tmp_test_directory_{__name__}").resolve()
-
-
+@pytest.fixture
 def set_up_test():
-    if test_directory().exists():
+    test_directory = Path(f"./tmp_test_directory_{__name__}").resolve()
+    if test_directory.exists():
         # Make sure the expected files has not been generated before and is still on disk due to e.g. a previous
         # test run having crashed:
-        shutil.rmtree(test_directory())
-    os.makedirs(test_directory())
-    test_file = test_directory().joinpath("TLSE00FRA_R_20230010000_10S_01S_MO.crx.gz")
+        shutil.rmtree(test_directory)
+    os.makedirs(test_directory)
+    test_file = test_directory.joinpath("TLSE00FRA_R_20230010000_10S_01S_MO.crx.gz")
     shutil.copy(prx.prx_root().joinpath(f"datasets/{test_file.name}"), test_file)
     assert test_file.exists()
-    return test_file
+    yield {"test_file": test_file}
+    shutil.rmtree(test_directory)
 
 
-def clean_up():
-    shutil.rmtree(test_directory())
-
-
-def test_find_local_ephemeris_file():
-    test_file = set_up_test()
-    aux_files = aux.get_on_it(test_file)
-    assert type(aux_files) is dict
-    shutil.rmtree(test_file.parent)
+def test_find_local_ephemeris_file(set_up_test):
+    aux_files = aux.get_on_it(set_up_test["test_file"])
