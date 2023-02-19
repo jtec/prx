4707c9174b76921e7ae593a26d355fb8114b3960
Jan Bolting <jan.bolting@swift-nav.com>
2023-02-19 17:17:46 +0100

    Applies first round of formatting and checks obs file time scale

--- minimumLovablePrototype/prx.py
+++ minimumLovablePrototype/prx.py
@@ -22,10 +22,10 @@ def prx_root() -> Path:
 
 
 def write_prx_file(
-        prx_header: dict,
-        prx_content: dict,
-        file_name_without_extension: Path,
-        output_format: str,
+    prx_header: dict,
+    prx_content: dict,
+    file_name_without_extension: Path,
+    output_format: str,
 ):
     output_writers = {"jsonseq": write_json_text_sequence_file, "csv": write_csv_file}
     if output_format not in output_writers.keys():
@@ -36,7 +36,7 @@ def write_prx_file(
 
 
 def write_json_text_sequence_file(
-        prx_header: dict, prx_content: dict, file_name_without_extension: Path
+    prx_header: dict, prx_content: dict, file_name_without_extension: Path
 ):
     indent = 2
     output_file = Path(
@@ -50,7 +50,7 @@ def write_json_text_sequence_file(
 
 
 def write_csv_file(
-        prx_header: dict, prx_content: dict, file_name_without_extension: Path
+    prx_header: dict, prx_content: dict, file_name_without_extension: Path
 ):
     output_file = Path(
         f"{str(file_name_without_extension)}.{constants.cPrxCsvFileExtension}"
@@ -72,11 +72,11 @@ def carrier_frequencies_hz():
     cf["R"]["L2"] = defaultdict(dict)
     for frequency_slot in range(-7, 12 + 1):
         cf["R"]["L1"][frequency_slot] = (
-                                                1602 + frequency_slot * 9 / 16
-                                        ) * constants.cHzPerMhz
+            1602 + frequency_slot * 9 / 16
+        ) * constants.cHzPerMhz
         cf["R"]["L2"][frequency_slot] = (
-                                                1246 + frequency_slot * 7 / 16
-                                        ) * constants.cHzPerMhz
+            1246 + frequency_slot * 7 / 16
+        ) * constants.cHzPerMhz
     # Glonass CDMA signals
     cf["R"]["L4"] = 1600.995 * constants.cHzPerMhz
     cf["R"]["L3"] = 1202.025 * constants.cHzPerMhz
@@ -122,15 +122,42 @@ def build_header(input_files):
     return prx_header
 
 
+def check_assumptions(rinex_3_obs_file):
+    obs_header = georinex.rinexheader(rinex_3_obs_file)
+    if "RCV CLOCK OFFS APPL" in obs_header.keys():
+        assert (
+                obs_header["RCV CLOCK OFFS APPL"].strip() == "0"
+        ), "Handling of 'RCV CLOCK OFFS APPL' != 0 not implemented yet."
+    assert (
+            obs_header["TIME OF FIRST OBS"].split()[-1].strip() == "GPS"
+    ), "Handling of observation files using time scales other than GPST no implemented yet"
+
+
 def build_records(rinex_3_obs_file, rinex_3_ephemerides_file):
+    check_assumptions(rinex_3_obs_file)
     obs = parse_rinex.load(rinex_3_obs_file, use_caching=True)
-    # First flatten the DataSet:
+
+    # Flatten the xarray DataSet into a pandas DataFrame:
     flat_obs = pd.DataFrame()
     for obs_label, sat_time_obs_array in obs.data_vars.items():
         df = sat_time_obs_array.to_dataframe(name="obs_value").reset_index()
-        df = df[df['obs_value'].notna()]
+        df = df[df["obs_value"].notna()]
         df = df.assign(obs_type=lambda x: obs_label)
         flat_obs = pd.concat([flat_obs, df])
+
+    def format(row):
+        return [
+            helpers.timestamp_2_gpst_ns(pd.Timestamp(row[0])),
+            str(row[1]),
+            row[2],
+            str(row[3]),
+        ]
+
+    flat_obs = flat_obs.apply(format, axis=1, result_type="expand")
+    flat_obs.rename(
+        columns={0: "rx_time_gpst_ns", 1: "satellite", 2: "obs_value", 3: "obs_type"},
+        inplace=True,
+    )
 
     nav = parse_rinex.load(rinex_3_ephemerides_file, use_caching=True)
 
@@ -149,14 +176,15 @@ def process(observation_file_path: Path, output_format="jsonseq"):
         build_records(rinex_3_obs_file, aux_files["broadcast_ephemerides"]),
         rinex_obs,
         prx_file,
-        output_format)
+        output_format,
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="prx",
         description="prx processes RINEX observations, computes a few useful things such as satellite position, "
-                    "relativistic effects etc. and outputs everything to a text file in a convenient format.",
+        "relativistic effects etc. and outputs everything to a text file in a convenient format.",
         epilog="P.S. GNSS rules!",
     )
     parser.add_argument(
@@ -171,7 +199,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     if (
-            args.observation_file_path is not None
-            and Path(args.observation_file_path).exists()
+        args.observation_file_path is not None
+        and Path(args.observation_file_path).exists()
     ):
         process(Path(args.observation_file_path), args.output_format)
