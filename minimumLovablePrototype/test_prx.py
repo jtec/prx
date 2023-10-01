801388628588882f652baa2215088f2d9d32ca4b
paul <paul.thevenon@gmail.com>
2023-10-01 23:45:27 +0200

    - added test for long input (01D: 1 day) to reveal bug on time of emission before the beginning of the day for 24h IGS files.

--- minimumLovablePrototype/test_prx.py
+++ minimumLovablePrototype/test_prx.py
@@ -9,6 +9,15 @@ import prx
 import helpers
 import constants
 
+def copy_data_file_to_test_directory(filepath,test_directory):
+    filename = filepath.split("/")[-1]
+    test_file = test_directory.joinpath(filename)
+    shutil.copy(
+        helpers.prx_root().joinpath(filepath),
+        test_file,
+    )
+    assert test_file.exists()
+    return test_file
 
 # This function sets up a temporary directory, copies a rinex observations file into that directory
 # and returns its path. The @pytest.fixture annotation allows us to pass the function as an input
@@ -23,15 +32,11 @@ def input_for_test():
         # test run having crashed:
         shutil.rmtree(test_directory)
     os.makedirs(test_directory)
-    compressed_compact_rinex_file = "TLSE00FRA_R_20230010100_10S_01S_MO.crx.gz"
-    test_file = test_directory.joinpath(compressed_compact_rinex_file)
-    shutil.copy(
-        helpers.prx_root().joinpath(
-            f"datasets/TLSE_2023001/{compressed_compact_rinex_file}"
-        ),
-        test_file,
-    )
-    assert test_file.exists()
+
+    compressed_compact_rinex_filepath = "datasets/TLSE_2023001//TLSE00FRA_R_20230010100_10S_01S_MO.crx.gz"
+    test_file = copy_data_file_to_test_directory(compressed_compact_rinex_filepath,
+                                                 test_directory, )
+
     # Also provide ephemerides so the test does not have to download them:
     ephemerides_file = "BRDC00IGS_R_20230010000_01D_MN.rnx.zip"
     shutil.copy(
@@ -42,6 +47,37 @@ def input_for_test():
 
     yield test_file
     shutil.rmtree(test_file.parent)
+
+@pytest.fixture
+def input_for_test_long_version():
+    test_directory = Path(f"./tmp_test_directory_{__name__}").resolve()
+    if test_directory.exists():
+        # Make sure the expected file has not been generated before and is still on disk due to e.g. a previous
+        # test run having crashed:
+        shutil.rmtree(test_directory)
+    os.makedirs(test_directory)
+
+    filepath_to_gps_obs_file = "datasets/TLSE_2022001/TLSE00FRA_R_20220010000_01D_30S_GO.zip"
+    test_gps_obs_file = copy_data_file_to_test_directory(filepath_to_gps_obs_file,
+                                                         test_directory,)
+
+    filepath_to_gps_nav_file = "datasets/TLSE_2022001/BRDC00IGS_R_20220010000_01D_GN.zip"
+    test_gps_nav_file = copy_data_file_to_test_directory(filepath_to_gps_nav_file,
+                                                         test_directory,)
+
+    filepath_to_mixed_obs_file = "datasets/TLSE_2022001/TLSE00FRA_R_20220010000_01D_30S_MO.zip"
+    test_mixed_obs_file = copy_data_file_to_test_directory(filepath_to_mixed_obs_file,
+                                                         test_directory,)
+
+    filepath_to_mixed_nav_file = "datasets/TLSE_2022001/BRDC00IGS_R_20220010000_01D_MN.zip"
+    test_mixed_nav_file = copy_data_file_to_test_directory(filepath_to_mixed_nav_file,
+                                                         test_directory,)
+
+    yield {"gps_obs_file": test_gps_obs_file,
+           "gps_nav_file": test_gps_nav_file,
+           "mixed_obs_file": test_mixed_obs_file,
+           "mixed_nav_file": test_mixed_nav_file,}
+    shutil.rmtree(test_directory)
 
 
 def test_prx_command_line_call_with_jsonseq_output(input_for_test):
@@ -69,6 +105,14 @@ def test_prx_function_call_with_jsonseq_output(input_for_test):
 
 def test_prx_function_call_with_csv_output(input_for_test):
     test_file = input_for_test
+    prx.process(observation_file_path=test_file, output_format="csv")
+    expected_prx_file = Path(
+        str(test_file).replace("crx.gz", constants.cPrxCsvFileExtension)
+    )
+    assert expected_prx_file.exists()
+
+def test_prx_function_call_with_csv_output_for_long_gps_rnx_file(input_for_test_long_version):
+    test_file = input_for_test_long_version["gps_obs_file"]
     prx.process(observation_file_path=test_file, output_format="csv")
     expected_prx_file = Path(
         str(test_file).replace("crx.gz", constants.cPrxCsvFileExtension)
