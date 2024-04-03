Fixes rinex_nav package benchmark + parallelises sat state computation + adds second nav file server (#68)
fixes rinex nav benchmark and adds method to parallelize
use parallel sat state computation
Add second nav file source (ftp)

src/prx/rinex_nav/nav_file_discovery.py
@@ -5,8 +5,6 @@ import georinex
 import urllib.request
 import glob
 import pandas as pd
-import numpy as np
-
 
 from prx import converters, helpers
 
@@ -17,40 +15,58 @@ def is_rinex_3_mixed_mgex_broadcast_ephemerides_file(file: Path):
     return str(file).endswith("MN.rnx")
 
 
-def try_downloading_ephemerides_from_bkg(
-    # This function returns a list of paths, in case the observation file spans over several days
-    t_start: pd.Timestamp,
-    t_end: pd.Timestamp,
-    folder: Path,
-):
-    files = []
-    time_starts = np.array(
-        [
-            pd.Timestamp(year=t_start.year, month=t_start.month, day=t_start.day),
-            pd.Timestamp(year=t_end.year, month=t_end.month, day=t_end.day),
-        ],
+def try_downloading_ephemerides_http(day: pd.Timestamp, folder: Path):
+    # IGS BKG Rinex 3.04 mixed file paths follow this pattern:
+    # For files after 2022
+    # https://igs.bkg.bund.de/root_ftp/IGS/BRDC/2023/002/BRDC00IGS_R_20230020000_01D_MN.rnx.gz
+    # For files before 2022
+    # https://igs.bkg.bund.de/root_ftp/IGS/BRDC/2021/365/BRDC00WRD_R_20213650000_01D_MN.rnx.gz
+    if day.year < 2022:
+        country_code = "WRD"
+    else:
+        country_code = "IGS"
+    remote_file = Path(
+        f"/{day.year}/{day.day_of_year:03}/BRDC00{country_code}_R_{day.year}{day.day_of_year:03}0000_01D_MN.rnx.gz"
     )
-    for time in time_starts:
-        # IGS BKG Rinex 3.04 mixed file paths follow this pattern:
-        # For files after 2022
-        # https://igs.bkg.bund.de/root_ftp/IGS/BRDC/2023/002/BRDC00IGS_R_20230020000_01D_MN.rnx.gz
-        # For files before 2022
-        # https://igs.bkg.bund.de/root_ftp/IGS/BRDC/2021/365/BRDC00WRD_R_20213650000_01D_MN.rnx.gz
-        if time.year < 2022:
-            country_code = "WRD"
-        else:
-            country_code = "IGS"
-        remote_file = Path(
-            f"/{time.year}/{time.day_of_year:03}/BRDC00{country_code}_R_{time.year}{time.day_of_year:03}0000_01D_MN.rnx.gz"
-        )
-        local_compressed_file = folder.joinpath(remote_file.name)
-        url = "https://igs.bkg.bund.de/root_ftp/IGS/BRDC" + str(remote_file.as_posix())
+    local_compressed_file = folder.joinpath(remote_file.name)
+    url = "https://igs.bkg.bund.de/root_ftp/IGS/BRDC" + str(remote_file.as_posix())
+    try:
         urllib.request.urlretrieve(url, local_compressed_file)
         local_file = converters.compressed_to_uncompressed(local_compressed_file)
         os.remove(local_compressed_file)
         log.info(f"Downloaded broadcast ephemerides file from {url}")
-        local_file = helpers.repair_with_gfzrnx(local_file)
-        files.append(local_file)
+        return local_file
+    except Exception:
+        log.warning(f"Could not download broadcast ephemerides file from {url}")
+        return None
+
+
+def try_downloading_ephemerides_ftp(day: pd.Timestamp, folder: Path):
+    root = "igs.ign.fr/pub/igs/data"
+    file_name = f"BRDM00DLR_S_{day.year}{day.day_of_year:03}0000_01D_MN.rnx.gz"
+    ftp_file_path = f"ftp://{root}/{day.year}/{day.day_of_year:03}/{file_name}"
+    local_compressed_file = folder / file_name
+    urllib.request.urlretrieve(ftp_file_path, local_compressed_file)
+    if not local_compressed_file.exists():
+        log.warning(f"Could not download {ftp_file_path}")
+        return None
+    local_file = converters.compressed_to_uncompressed(local_compressed_file)
+    os.remove(local_compressed_file)
+    log.info(f"Downloaded broadcast ephemerides file {ftp_file_path}")
+    return local_file
+
+
+def try_downloading_ephemerides(t_start: pd.Timestamp, t_end, folder: Path):
+    time = t_start
+    files = []
+
+    local_file = try_downloading_ephemerides_http(time, folder)
+    if not local_file:
+        local_file = try_downloading_ephemerides_ftp(time, folder)
+    assert local_file, f"Could not download broadcast ephemerides for {time}"
+
+    local_file = helpers.repair_with_gfzrnx(local_file)
+    files.append(local_file)
     if len(files) == 0:
         return None
     else:
@@ -97,7 +113,7 @@ def discover_local_ephemerides(
 def discover_or_download_ephemerides(
     t_start: pd.Timestamp, t_end: pd.Timestamp, folder, constellations
 ):
-    sources = [discover_local_ephemerides, try_downloading_ephemerides_from_bkg]
+    sources = [discover_local_ephemerides, try_downloading_ephemerides]
     for source in sources:
         ephemerides_files = source(t_start, t_end, folder)
         if ephemerides_files is not None: