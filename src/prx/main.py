58a63ee1c7a00dad2849855bd43ce4079abcc27a
janbolting <jan.bolting@gmail.com>
2024-04-05 23:35:10 +0200

    ruff format

--- src/prx/main.py
+++ src/prx/main.py
@@ -15,10 +15,10 @@ log = helpers.get_logger(__name__)
 
 
 def write_prx_file(
-        prx_header: dict,
-        prx_records: pd.DataFrame,
-        file_name_without_extension: Path,
-        output_format: str,
+    prx_header: dict,
+    prx_records: pd.DataFrame,
+    file_name_without_extension: Path,
+    output_format: str,
 ):
     output_writers = {"jsonseq": write_json_text_sequence_file, "csv": write_csv_file}
     if output_format not in output_writers.keys():
@@ -27,7 +27,7 @@ def write_prx_file(
 
 
 def write_json_text_sequence_file(
-        prx_header: dict, prx_records: pd.DataFrame, file_name_without_extension: Path
+    prx_header: dict, prx_records: pd.DataFrame, file_name_without_extension: Path
 ):
     output_file = Path(
         f"{str(file_name_without_extension)}.{constants.cPrxJsonTextSequenceFileExtension}"
@@ -43,7 +43,7 @@ def write_json_text_sequence_file(
             epoch = pd.Timestamp(epoch)
             epoch_obs = prx_records[
                 prx_records["time_of_reception_in_receiver_time"] == epoch
-                ]
+            ]
             record = {
                 "time_of_reception_in_receiver_time": epoch.strftime(
                     "%Y:%m:%dT%H:%M:%S.%f"
@@ -70,7 +70,7 @@ def write_json_text_sequence_file(
 
 
 def write_csv_file(
-        prx_header: dict, flat_records: pd.DataFrame, file_name_without_extension: Path
+    prx_header: dict, flat_records: pd.DataFrame, file_name_without_extension: Path
 ):
     output_file = Path(
         f"{str(file_name_without_extension)}.{constants.cPrxCsvFileExtension}"
@@ -162,22 +162,22 @@ def build_metadata(input_files):
 
 
 def check_assumptions(
-        rinex_3_obs_file,
+    rinex_3_obs_file,
 ):
     obs_header = georinex.rinexheader(rinex_3_obs_file)
     if "RCV CLOCK OFFS APPL" in obs_header.keys():
         assert (
-                obs_header["RCV CLOCK OFFS APPL"].strip() == "0"
+            obs_header["RCV CLOCK OFFS APPL"].strip() == "0"
         ), "Handling of 'RCV CLOCK OFFS APPL' != 0 not implemented yet."
     assert (
-            obs_header["TIME OF FIRST OBS"].split()[-1].strip() == "GPS"
+        obs_header["TIME OF FIRST OBS"].split()[-1].strip() == "GPS"
     ), "Handling of observation files using time scales other than GPST not implemented yet."
 
 
 def build_records(
-        rinex_3_obs_file,
-        rinex_3_ephemerides_files,
-        approximate_receiver_ecef_position_m,
+    rinex_3_obs_file,
+    rinex_3_ephemerides_files,
+    approximate_receiver_ecef_position_m,
 ):
     return _build_records_cached(
         rinex_3_obs_file,
@@ -193,11 +193,11 @@ def build_records(
 
 @helpers.cache_call
 def _build_records_cached(
-        rinex_3_obs_file,
-        rinex_3_obs_file_hash,
-        rinex_3_ephemerides_files,
-        rinex_3_ephemerides_file_hash,
-        approximate_receiver_ecef_position_m,
+    rinex_3_obs_file,
+    rinex_3_obs_file_hash,
+    rinex_3_ephemerides_files,
+    rinex_3_ephemerides_file_hash,
+    approximate_receiver_ecef_position_m,
 ):
     approximate_receiver_ecef_position_m = np.array(
         approximate_receiver_ecef_position_m
@@ -312,16 +312,16 @@ def _build_records_cached(
                 file,
                 query.loc[
                     (
-                            query.query_time_isagpst
-                            >= pd.Timestamp(year=year, month=1, day=1)
-                            + pd.Timedelta(days=doy - 1)
+                        query.query_time_isagpst
+                        >= pd.Timestamp(year=year, month=1, day=1)
+                        + pd.Timedelta(days=doy - 1)
                     )
                     & (
-                            query.query_time_isagpst
-                            < pd.Timestamp(year=year, month=1, day=1)
-                            + pd.Timedelta(days=doy)
+                        query.query_time_isagpst
+                        < pd.Timestamp(year=year, month=1, day=1)
+                        + pd.Timedelta(days=doy)
                     )
-                    ],
+                ],
             )
         )
     sat_states = pd.concat(sat_states_per_day)
@@ -415,7 +415,7 @@ def _build_records_cached(
             return np.nan
         return constants.carrier_frequencies_hz()[row.satellite[0]][
             "L" + row.observation_type[1]
-            ][row["frequency_slot"]]
+        ][row["frequency_slot"]]
 
     flat_obs.loc[:, "carrier_frequency_hz"] = flat_obs.apply(
         signal_2_carrier_frequency, axis=1
@@ -436,12 +436,12 @@ def _build_records_cached(
 
         # Selection criteria: time of emission belonging to the day of the current NAV file
         mask = (
-                       flat_obs.time_of_emission_isagpst
-                       >= pd.Timestamp(year=year, month=1, day=1) + pd.Timedelta(days=doy - 1)
-               ) & (
-                       flat_obs.time_of_emission_isagpst
-                       < pd.Timestamp(year=year, month=1, day=1) + pd.Timedelta(days=doy)
-               )
+            flat_obs.time_of_emission_isagpst
+            >= pd.Timestamp(year=year, month=1, day=1) + pd.Timedelta(days=doy - 1)
+        ) & (
+            flat_obs.time_of_emission_isagpst
+            < pd.Timestamp(year=year, month=1, day=1) + pd.Timedelta(days=doy)
+        )
 
         flat_obs.loc[
             mask,
@@ -455,9 +455,9 @@ def _build_records_cached(
             latitude_user_rad,
             longitude_user_rad,
         ) * (
-                    constants.carrier_frequencies_hz()["G"]["L1"][1] ** 2
-                    / flat_obs.loc[mask].carrier_frequency_hz ** 2
-            )
+            constants.carrier_frequencies_hz()["G"]["L1"][1] ** 2
+            / flat_obs.loc[mask].carrier_frequency_hz ** 2
+        )
 
     return flat_obs
 
@@ -493,7 +493,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="prx",
         description="prx processes RINEX observations, computes a few useful things such as satellite position, "
-                    "relativistic effects etc. and outputs everything to a text file in a convenient format.",
+        "relativistic effects etc. and outputs everything to a text file in a convenient format.",
         epilog="P.S. GNSS rules!",
     )
     parser.add_argument(
@@ -508,7 +508,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     if (
-            args.observation_file_path is not None
-            and Path(args.observation_file_path).exists()
+        args.observation_file_path is not None
+        and Path(args.observation_file_path).exists()
     ):
         process(Path(args.observation_file_path), args.output_format)
