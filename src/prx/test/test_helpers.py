9f10ac1ea0d182a4a517048c04f4c78ae91e4fd1
Jan Bolting <jan.bolting@gmail.com>
2024-07-28 16:11:56 +0200

    fix failing tests

--- src/prx/test/test_helpers.py
+++ src/prx/test/test_helpers.py
@@ -180,7 +180,8 @@ def test_satellite_elevation_and_azimuth():
 def test_sagnac_effect():
     # load validation data
     path_to_validation_file = (
-        helpers.prx_repository_root() / "tools/validation_data/sagnac_effect.csv"
+        helpers.prx_repository_root()
+        / "src/prx/tools/validation_data/sagnac_effect.csv"
     )
 
     # satellite position (from reference CSV header)
