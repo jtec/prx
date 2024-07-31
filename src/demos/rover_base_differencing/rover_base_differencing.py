97457a9d286f83601ce9a724d8788628a27aacc2
Jan Bolting <jan.bolting@gmail.com>
2024-07-31 12:15:01 +0200

    silence linter

--- src/demos/rover_base_differencing/rover_base_differencing.py
+++ src/demos/rover_base_differencing/rover_base_differencing.py
@@ -24,6 +24,8 @@ def main():
         prx_process(rx_rover_obs)
     df_rover = pd.read_csv(rx_rover_obs.with_suffix(".csv"))
     df_base = pd.read_csv(rx_base_obs.with_suffix(".csv"))
+    print(df_rover.info())
+    print(df_base.info())
 
 
 if __name__ == "__main__":
