13a409f29cd252652b9d2e9e77f628dc0484deaf
Jan Bolting <jan.bolting@gmail.com>
2024-08-11 17:59:00 +0200

    SDs done

--- src/demos/rover_base_differencing/rover_base_differencing.py
+++ src/demos/rover_base_differencing/rover_base_differencing.py
@@ -1,31 +1,43 @@
 from pathlib import Path
-import pandas as pd
 
 from prx import helpers
-from prx.main import process as prx_process
 from prx.converters import anything_to_rinex_3
+from prx.user import parse_prx_csv_file
 
 log = helpers.get_logger(__name__)
+
+
+def parse(obs_file: Path):
+    rnx_file = anything_to_rinex_3(obs_file)
+    df, metadata = parse_prx_csv_file(rnx_file.with_suffix(".csv"))
+    # Receiver clocks are off by at most a few tens of milliseconds, so let's round for matching rover-base obs
+    df["time_of_reception_rounded"] = df.time_of_reception_in_receiver_time.round(
+        "50ms"
+    )
+    df["sv"] = df["constellation"].astype(str) + df["prn"].astype(str).str.pad(
+        2, fillchar="0"
+    )
+    return df
 
 
 # Provides and example of how to compute between-receivers single
 # differences and double differences after processing obs files with prx.
 def main():
     # We'll use two IGS stations that are just roughly a kilometer apart
-    rx_base_obs = anything_to_rinex_3(
-        Path(__file__).parent / "obs/TLSE00FRA_R_20241900000_15M_01S_MO.crx.gz"
+    df_rover, df_base = (
+        parse(Path(__file__).parent / "obs/TLSE00FRA_R_20241900000_15M_01S_MO.crx.gz"),
+        parse(Path(__file__).parent / "obs/TLSG00FRA_R_20241900000_15M_01S_MO.crx.gz"),
     )
-    rx_rover_obs = anything_to_rinex_3(
-        Path(__file__).parent / "obs/TLSG00FRA_R_20241900000_15M_01S_MO.crx.gz"
-    )
-    if not rx_base_obs.with_suffix(".csv").exists():
-        prx_process(rx_base_obs)
-    if not rx_rover_obs.with_suffix(".csv").exists():
-        prx_process(rx_rover_obs)
-    df_rover = pd.read_csv(rx_rover_obs.with_suffix(".csv"))
-    df_base = pd.read_csv(rx_base_obs.with_suffix(".csv"))
-    print(df_rover.info())
-    print(df_base.info())
+    df = df_rover.merge(
+        df_base,
+        on=["time_of_reception_rounded", "sv", "rnx_obs_identifier"],
+        suffixes=("_rover", "_base"),
+    ).reset_index()
+    df["C_obs_m_sd"] = df.C_obs_m_rover + df.C_obs_m_base
+    df["L_obs_cycles_sd"] = df.L_obs_cycles_rover + df.L_obs_cycles_base
+    df["ref_sv"] = ""
+
+    print(df.info())
 
 
 if __name__ == "__main__":
