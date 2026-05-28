16b7cfcaf4fbdd14a9be958bce48a72171d8ea2d
Paul Thevenon <thevenon@recherche.enac.fr>
2026-05-29 00:53:14 +0200

    format

--- src/prx/user.py
+++ src/prx/user.py
@@ -284,31 +284,35 @@ def bootstrap_coarse_receiver_position(filepath_obs, filepath_nav):
     )
 
     # create obs df by merging query and sat_states
-    obs_df = query[
-        [
-            "time_of_reception_in_receiver_time",
-            "sv",
-            "observation_value",
-        ]
-    ].merge(
-        sat_states[
+    obs_df = (
+        query[
             [
-                "sat_code_bias_m",
-                "sat_clock_offset_m",
-                "sat_clock_drift_mps",
+                "time_of_reception_in_receiver_time",
                 "sv",
-                "sat_pos_x_m",
-                "sat_pos_y_m",
-                "sat_pos_z_m",
-                "sat_vel_x_mps",
-                "sat_vel_y_mps",
-                "sat_vel_z_mps",
-                "relativistic_clock_effect_m",
+                "observation_value",
             ]
-        ],
-        how="left",
-        on="sv",
-    ).dropna()
+        ]
+        .merge(
+            sat_states[
+                [
+                    "sat_code_bias_m",
+                    "sat_clock_offset_m",
+                    "sat_clock_drift_mps",
+                    "sv",
+                    "sat_pos_x_m",
+                    "sat_pos_y_m",
+                    "sat_pos_z_m",
+                    "sat_vel_x_mps",
+                    "sat_vel_y_mps",
+                    "sat_vel_z_mps",
+                    "relativistic_clock_effect_m",
+                ]
+            ],
+            how="left",
+            on="sv",
+        )
+        .dropna()
+    )
     obs_df = obs_df.rename(columns={"observation_value": "C_obs_m"})
     # add missing columns with 0 value, since approximate position is unknown
     obs_df = pd.concat(
