c205851787bdb2a3b2882c202d40f3edfeeff67b
Jan Bolting <jan.bolting@gmail.com>
2026-06-04 18:42:53 +0200

    format

--- src/prx/test/test_util.py
+++ src/prx/test/test_util.py
@@ -13,7 +13,26 @@ log = logging.getLogger(__name__)
 
 
 def test_timedelta_2_seconds():
-    expected_timedelta_s = cSecondsPerDay +  1.23456789
-    assert np.isclose(timedelta_2_seconds(pd.Timedelta(days=1, seconds=1.23456789)), expected_timedelta_s, atol=1e-9)
-    assert np.isclose(timedelta_2_seconds(pd.Series([pd.Timedelta(days=1, seconds=1.23456789)])).iloc[0], expected_timedelta_s, atol=1e-9)
-    assert np.isclose(timedelta_2_seconds(pl.Series(values=[timedelta(days=1, seconds=1.23456789)], dtype=pl.Duration(time_unit="ns")))[0], expected_timedelta_s, atol=1e-9)
+    expected_timedelta_s = cSecondsPerDay + 1.23456789
+    assert np.isclose(
+        timedelta_2_seconds(pd.Timedelta(days=1, seconds=1.23456789)),
+        expected_timedelta_s,
+        atol=1e-9,
+    )
+    assert np.isclose(
+        timedelta_2_seconds(pd.Series([pd.Timedelta(days=1, seconds=1.23456789)])).iloc[
+            0
+        ],
+        expected_timedelta_s,
+        atol=1e-9,
+    )
+    assert np.isclose(
+        timedelta_2_seconds(
+            pl.Series(
+                values=[timedelta(days=1, seconds=1.23456789)],
+                dtype=pl.Duration(time_unit="ns"),
+            )
+        )[0],
+        expected_timedelta_s,
+        atol=1e-9,
+    )
