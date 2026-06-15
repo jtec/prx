5dd689f1859086ae4fa9fb25445fadf6609f59f2
Jan Bolting <jan.bolting@gmail.com>
2026-06-15 21:03:49 +0200

    hop

--- src/prx/test/benchmark_copying.py
+++ src/prx/test/benchmark_copying.py
@@ -1,4 +1,3 @@
-import os
 import time
 import tracemalloc
 
@@ -7,31 +6,41 @@ import pandas as pd
 import polars as pl
 from string import ascii_uppercase
 
+
+def wraps_polars(df: pd.DataFrame):
+    pl_df = pl.from_pandas(df)
+    pl_df = pl_df.select(pl.all() * 2)
+    return pl_df.to_pandas(
+        split_blocks=True,
+        self_destruct=True,
+    )
+
+
 def main():
-    array_bytes = 1e6
+    array_bytes = 1e9
     n_columns = 10
     np_datatype = np.float64
     n_rows = int((array_bytes / n_columns) / 8)
-
     time.sleep(1)
-    pandas_df = pd.DataFrame({column_name: np.random.random(n_rows).astype(np_datatype) for column_name in ascii_uppercase[:n_columns]})
 
+    pandas_df = pd.DataFrame(
+        {
+            column_name: np.random.random(n_rows).astype(np_datatype)
+            for column_name in ascii_uppercase[:n_columns]
+        }
+    )
     time.sleep(1)
-    #pandas_df = wraps_polars(pandas_df)
-    polars_df = pl.from_pandas(pandas_df)
-    polars_df = polars_df * 2
 
+    pandas_df = wraps_polars(pandas_df)
     time.sleep(1)
-    pandas_df = polars_df.to_pandas()
 
-    time.sleep(1)
-    input(f"Press Enter to exit process ({os.getpid()}) ...")
+    # input(f"Press Enter to exit process ({os.getpid()}) ...")
 
 
 if __name__ == "__main__":
-  tracemalloc.start()
-  main()
-  time.sleep(1)
-  current, peak = tracemalloc.get_traced_memory()
-  print(f"tracemalloc current : {current / 10**6} MB")
-  print(f"tracemalloc peak: {peak / 10**6} MB")
+    tracemalloc.start()
+    main()
+    time.sleep(1)
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"tracemalloc current : {current / 10**6} MB")
+    print(f"tracemalloc peak: {peak / 10**6} MB")
