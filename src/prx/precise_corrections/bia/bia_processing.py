f5089a3d550bee54986bae920ab8489ddbc73c0c
Paul Thevenon <thevenon@recherche.enac.fr>
2026-08-07 16:03:02 +0200

    - add caching

--- src/prx/precise_corrections/bia/bia_processing.py
+++ src/prx/precise_corrections/bia/bia_processing.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import polars as pl
-from prx import constants, converters
+from prx import constants, converters, util
 
 
 def parse_bia_file(filepath_bia_gz: Path) -> pl.DataFrame:
@@ -27,39 +27,47 @@ def parse_bia_file(filepath_bia_gz: Path) -> pl.DataFrame:
     └────────┴────────┴───────────────┘
 
     """
-    filepath_bia = converters.compressed_to_uncompressed(filepath_bia_gz)
-    with open(filepath_bia, "r") as f:
-        sat_id_list = []
-        obs1_list = []
-        val_list = []
-        # find beginning of block BIAS/SOLUTION
-        for line in f:
-            if line.startswith("+BIAS/SOLUTION"):
-                break
 
-        for line in f:
-            if line.startswith("-BIAS/SOLUTION"):  # escape loop
-                break
-            if line.startswith("*BIAS"):  # skip header
-                continue
-            bias_type = line[0:4].strip()
-            station = line[15:24].strip()
-            if (station == "") and (
-                bias_type == "OSB"
-            ):  # keep only OSB and satellite biases
-                unit = line[64:69].strip()
-                assert unit == "ns", f"Wrong unit in file. Expected 'ns', read '{unit}'"
-                sat_id = line[11:14].strip()
-                obs1 = line[25:29].strip()
-                estimated_value = (
-                    float(line[70:91])
-                    / constants.cNanoSecondsPerSecond
-                    * constants.cGpsSpeedOfLight_mps
-                )
-                sat_id_list.append(sat_id)
-                obs1_list.append(obs1)
-                val_list.append(estimated_value)
-        bia_df = pl.DataFrame(
-            {"sat_id": sat_id_list, "obs_id": obs1_list, "sat_hw_bias_m": val_list}
-        )
-        return bia_df
+    @util.disk_cache.cache(ignore=["filepath_bia_gz"])
+    def cached_load(filepath_bia_gz: Path, file_hash: str):
+        filepath_bia = converters.compressed_to_uncompressed(filepath_bia_gz)
+        with open(filepath_bia, "r") as f:
+            sat_id_list = []
+            obs1_list = []
+            val_list = []
+            # find beginning of block BIAS/SOLUTION
+            for line in f:
+                if line.startswith("+BIAS/SOLUTION"):
+                    break
+
+            for line in f:
+                if line.startswith("-BIAS/SOLUTION"):  # escape loop
+                    break
+                if line.startswith("*BIAS"):  # skip header
+                    continue
+                bias_type = line[0:4].strip()
+                station = line[15:24].strip()
+                if (station == "") and (
+                    bias_type == "OSB"
+                ):  # keep only OSB and satellite biases
+                    unit = line[64:69].strip()
+                    assert unit == "ns", (
+                        f"Wrong unit in file. Expected 'ns', read '{unit}'"
+                    )
+                    sat_id = line[11:14].strip()
+                    obs1 = line[25:29].strip()
+                    estimated_value = (
+                        float(line[70:91])
+                        / constants.cNanoSecondsPerSecond
+                        * constants.cGpsSpeedOfLight_mps
+                    )
+                    sat_id_list.append(sat_id)
+                    obs1_list.append(obs1)
+                    val_list.append(estimated_value)
+            bia_df = pl.DataFrame(
+                {"sat_id": sat_id_list, "obs_id": obs1_list, "sat_hw_bias_m": val_list}
+            )
+            return bia_df
+
+    file_content_hash = util.hash_of_file_content(filepath_bia_gz)
+    return cached_load(filepath_bia_gz, file_content_hash)
